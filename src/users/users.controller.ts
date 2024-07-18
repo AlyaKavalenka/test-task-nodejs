64fb55a988dedb3454cf2b7a969cde46356e19fa
Alya Kavalenka <kovalenkoalinam@gmail.com>
2024-07-18 13:57:04 +0300

    feat: owner guard for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { Roles } from 'src/auth/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { OwnerGuard } from 'src/auth/owner.guard';
 
 @Controller('users')
 export class UsersController {
@@ -24,21 +25,21 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard, OwnerGuard)
   @Roles('admin', 'user')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard, OwnerGuard)
   @Roles('admin', 'user')
   @Put(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard, OwnerGuard)
   @Roles('admin', 'user')
   @Delete(':id')
   remove(@Param('id') id: string) {
